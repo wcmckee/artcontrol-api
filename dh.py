d9e0cafb1a481208286c5fc244cd6d7e3409d063
wcmckee <will@artcontrol.me>
2014-03-10 01:47:05 +0000

    dhcpd start

---
+++ dh.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=2>
+
+# dhcpd.leases
+
+# <codecell>
+
+ls
+
+# <codecell>
+
+dh = open('dhcpd.leases', 'r')
+
+# <codecell>
+
+for f in dh:
+    print f
+
+# <codecell>
+
+
