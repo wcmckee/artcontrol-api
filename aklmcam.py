d5f3da86f1094eff193a37488dd0d060bed4ff2a
wcmckee <will@artcontrol.me>
2014-05-07 07:11:31 +1200

    removed first open/write

--- aklmcam.py
+++ aklmcam.py
@@ -4,7 +4,6 @@
 # <codecell>
 
 import subprocess
-import json
 
 # <codecell>
 
@@ -24,9 +23,9 @@ import xmltodict
 
 # <codecell>
 
-savxml = open('/home/will/Desktop/brobeur-static/feeds/aklmcam.json', 'w')
-savxml.write(data)
-savxml.close()
+#savxml = open('/home/will/Desktop/brobeur-static/feeds/aklmcam.json', 'w')
+#savxml.write(data)
+#savxml.close()
 
 # <codecell>
 
@@ -34,17 +33,21 @@ datadict = xmltodict.parse(data)
 
 # <codecell>
 
-newdc = json.dumps(datadict)
+import json
+
+#write the dictionary to a file
+outfile = open('linkz', 'w')
+json.dump(datadict, outfile)
+
+#read the data back in
+with open('linkz') as infile:
+    newDictionary = json.load(infile)
 
 # <codecell>
 
 savcdat = open('/home/will/Desktop/brobeur-static/feeds/aklmcam.json', 'w')
-savcdat.write(str(newdc))
+savcdat.write(str(newDictionary))
 savcdat.close()
-
-# <codecell>
-
-print ' done!'
 
 # <codecell>
 
